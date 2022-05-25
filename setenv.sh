f73ec8f74aa664c716d056ba643add3bd7261840
Mike Fulton <mikefultonpersonal@gmail.com>
2022-05-24 20:05:25 -0400

    get very primitive baseline down

---
+++ setenv.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Set up environment variables for general build tool to operate
+#
+if ! [ -f ./setenv.sh ]; then
+	echo "Need to source from the setenv.sh directory" >&2
+	return 0
+fi
+
+export PORT_ROOT="${PWD}"
+export PORT_TYPE="TARBALL"
+
+export PORT_TARBALL_URL="https://www.openssl.org/source/openssl-3.0.3.tar.gz"
+export PORT_TARBALL_DEPS="curl gzip make m4 perl"
+
+export PORT_GIT_URL="https://github.com/openssl/openssl"
+export PORT_GIT_DEPS="git make m4 perl autoconf automake help2man makeinfo xz"
+
+export PORT_EXTRA_CFLAGS=""
+export PORT_EXTRA_LDFLAGS=""
+
+if [ "${PORT_TYPE}x" = "TARBALLx" ]; then
+	export PORT_BOOTSTRAP=skip
+fi
+export PORT_CONFIGURE="./Configure"
