25928624fa2547c54398844cee57f29f55720522
Mike Fulton <mikefultonpersonal@gmail.com>
2022-06-16 20:29:03 -0400

    shift to left to remove 'diff' character

--- patches/PR0/tools/c99.sh
+++ patches/PR0/tools/c99.sh
@@ -1,16 +1,16 @@
-+#!/bin/sh -k
-+#
-+# Re-order arguments so that -L comes first
-+#
-+opts=""
-+lopts=""
-+        
-+for arg in $* ; do
-+  case $arg in
-+    -L*) lopts="$lopts $arg" ;;
-+    *) opts="$opts $arg" ;;
-+  esac
-+done
-+
-+c99 -Wl,dll $lopts $opts
-+#c99 -DOPENSSL_THREADS -D_OPEN_THREADS -O -Wc,XPLINK -DB_ENDIAN -DCHARSET_EBCDIC -DNO_SYS_PARAM_H -D_ALL_SOURCE -qlongname -qlanglvl=extc99 $lopts $opts
+#!/bin/sh -k
+#
+# Re-order arguments so that -L comes first
+#
+opts=""
+lopts=""
+        
+for arg in $* ; do
+  case $arg in
+    -L*) lopts="$lopts $arg" ;;
+    *) opts="$opts $arg" ;;
+  esac
+done
+
+c99 -Wl,dll $lopts $opts
+#c99 -DOPENSSL_THREADS -D_OPEN_THREADS -O -Wc,XPLINK -DB_ENDIAN -DCHARSET_EBCDIC -DNO_SYS_PARAM_H -D_ALL_SOURCE -qlongname -qlanglvl=extc99 $lopts $opts
