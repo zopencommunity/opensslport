d9b8ad5e064997729c339f25d285c9bce2255adc
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:35 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'opensslport'), string(name: 'DESCRIPTION', 'OpenSSL is a software library for applications that secure communications over computer networks against eavesdropping or need to identify the party at the other end.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'opensslport'), string(name: 'DESCRIPTION', value: 'OpenSSL is a software library for applications that secure communications over computer networks against eavesdropping or need to identify the party at the other end.' )]
         }
 }
