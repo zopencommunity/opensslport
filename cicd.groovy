6f874b2801218741813df6003653122d2fd2bf59
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:32 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/opensslport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/opensslport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/opensslport.git'), string(name: 'PORT_DESCRIPTION', value: 'OpenSSL is a software library for applications that secure communications over computer networks against eavesdropping or need to identify the party at the other end.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/opensslport.git'), string(name: 'PORT_DESCRIPTION', value: 'OpenSSL is a software library for applications that secure communications over computer networks against eavesdropping or need to identify the party at the other end.' )]
         }
 }
